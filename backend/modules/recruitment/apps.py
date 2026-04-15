bbbb425f42403d88f42523e712b582466aa3485f
ER-DipanshuSaini <contactmedipanshu@gmail.com>
2026-04-16 00:02:18 +0530

    chore: add documentation for recruitment module configuration

--- backend/modules/recruitment/apps.py
+++ backend/modules/recruitment/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 class RecruitmentConfig(AppConfig):
+    """
+    Configuration for the Recruitment module, handling job positions and applications.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'modules.recruitment'
     verbose_name = 'Recruitment Management'
